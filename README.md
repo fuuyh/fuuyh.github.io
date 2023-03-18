19b0fe7ba38e60161bfe480b26d69b41054c2b72
fuuyh <1849521093@qq.com>
2023-03-19 01:23:42 +0800

    clean

--- README.md
+++
@@ -1,1 +0,0 @@
-# fuuuyuhao.github.io
--- js/index.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-  //打开sidebar 
-  $('.hamRotate').click(function () {
-    $('.sidebar').animate({
-      width: 'toggle'
-    }, 200);
-    $('body').toggleClass("boom_body");
-  });
-});
