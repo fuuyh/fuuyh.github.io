6fdd2766121957ea772a1aec284deb021ae105e6
fuuyh <1849521093@qq.com>
2023-04-10 21:43:26 +0800

    v2023041001

--- js/index.js
+++ js/index.js
@@ -1,9 +1,39 @@
-$(document).ready(function () {
-  //打开sidebar 
-  $('.hamRotate').click(function () {
-    $('.sidebar').animate({
-      width: 'toggle'
-    }, 200);
-    $('body').toggleClass("boom_body");
-  });
-});
+// 内容数组
+let buttonLists = [
+  {
+    img: './images/github.svg',
+    title: 'Github',
+    link: 'https://github.com/fuuyh'
+  },
+  {
+    img: './images/blog.svg',
+    title: 'Blog',
+    link: 'https://www.noovertime.cc/blog'
+  },
+  {
+    img: './images/case.svg',
+    title: 'Case',
+    link: 'https://www.noovertime.cc/case/index.html'
+  },
+  {
+    img: './images/codepen.svg',
+    title: 'Codepen',
+    link: 'https://github.com/mcmyth'
+  },
+  {
+    img: './images/aboutme.svg',
+    title: 'About',
+    link: 'https://github.com/about.html'
+  }
+]
+
+// 遍历数组中的内容添加到节点里
+buttonLists.forEach((item) => {
+  $('#buttonList').append(`
+    <li>
+      <a href="${item.link}">
+        <img src="${item.img}" alt="${item.title}">
+      </a>
+    </li>
+  `) 
+})
