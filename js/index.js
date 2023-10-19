3d4df716c3dfe4d036743135723a5b31816605be
fuuyh <1849521093@qq.com>
2023-10-19 11:28:57 +0800

    v2023101902

--- js/index.js
+++ js/index.js
@@ -11,7 +11,7 @@ let buttonLists = [
     link: 'https://fuuyh.github.io/blog'
   },
   {
-    img: './images/case.svg',
+    img: './images/case.svg', 
     title: 'About',
     link: 'https://fuuyh.github.io/some-case'
   },
